719e99f685ed8f01f2734bbddbec2a776fd752a4
Naamah24 <54554741+Naamah24@users.noreply.github.com>
2023-08-12 23:23:59 +0300

    created custom hook to fetch games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,9 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "3c0a70a1064a4b329adb49f54f93b2c4", // rawg api key
+    key: "0a616af3e25e4a46be26f788552fc310", // rawg api key
   },
 });
+
+// apiKey is only for FE dev processes.
+// using a BE service it should be stored safely on the env vars
