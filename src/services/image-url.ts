6e20bc01c73960a2951cd9d76955f042acb77a0a
Naamah24 <54554741+Naamah24@users.noreply.github.com>
2023-08-15 14:47:36 +0300

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+// placeholder is a static asset deployed with our application so it needs to be imported like a module
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp";
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
